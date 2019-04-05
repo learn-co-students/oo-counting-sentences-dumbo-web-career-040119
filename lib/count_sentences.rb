05c7f66c8fba9b4069957e6956379c20d4943007
github username <github email address>
2019-04-05 19:44:32 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?('.')
   end
 
   def question?
-
+    self.include?('?')
   end
 
   def exclamation?
-
+    self.include?('!')
   end
+
 
   def count_sentences
-
+    #first we are splitting our sentence up into an array. Then using the delete_if method to make sure that there isn't a word in our array that isn't less that 1 letter
+    self.split(/\.|\?|\!/).delete_if {|word| word.length < 1}.length
   end
+
 end
