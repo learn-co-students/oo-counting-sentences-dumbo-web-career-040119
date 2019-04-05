c115e52f2e26d0330b58c8a2addcfba94bf46bfa
github username <github email address>
2019-04-04 21:08:43 -0400

    finished

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #binding.pry
+    split = self.split("")
+    split[length-1] == "."
   end
 
   def question?
-
+    split = self.split("")
+    split[length-1] == "?"
   end
 
   def exclamation?
-
+    split = self.split("")
+    split[length-1] == "!"
   end
 
   def count_sentences
-
+    # period = []
+    # sentence_count = 0
+    # split = self.split(" ")
+    # binding.pry
+    # return split.length
+    return self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
